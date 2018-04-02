085fb67bd3302cb9dc2c5dc36ad87ac0da258f12
maerli <maerli.pereira@gmail.com>
2018-04-03 03:46:05 +1000

    adicionando constructor e método map

--- Matrix.js
+++ Matrix.js
@@ -1,4 +1,15 @@
 class Matrix{
-
+  constructor(nrow,ncol){
+    this.nrow = nrow;
+    this.ncol = ncol;
+    this.data = this.map((x,y) => 0);
+  }
+  map(fun){
+    for(let i = 0;i<this.nrow;i++){
+      for(let j = 0;j<this.ncol;j++){
+        this[i][j] = fun(i,j);
+      }
+    } 
+  }
 
 }
